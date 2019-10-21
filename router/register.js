678490dc48072b111f2c469ab2d3ade61a1ffbf3
Илья Нидзельский <inidzelsky@MacBook-Air.local>
2019-10-21 17:40:09 +0300

    JSON Web Token initialised, added comentaries in register.js

--- router/register.js
+++ router/register.js
@@ -5,44 +5,75 @@ const path = require("path");
 const express = require("express");
 const { check, validationResult } = require('express-validator');
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 //Secondary Dependencies
 const User = require(path.join(__dirname, "..", "models", "User.js"));
 
+//Router init
 const router = express.Router();
 
 //@method POST /api/register
 //@desc Register a user
 //@access Public
-router.post('/', [
+router.post('/', [ 
+  //Request data validation
   check('email', 'Please, enter a valid email').isEmail(), 
   check('password', 'Please, enter a password with 6 or more characters').isLength({min: 6}),
 ], async (req, res) => {
+  //Checking errors existance
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({errors: errors.array()});
   }
 
   try {
+    //Getting credentials
     const { name, email, password } = req.body;
+
+    //Checking whether the user with the email exists
     let user = await User.findOne({email});
     if (user) {
       return res.status(400).json({msg: `User with the email: ${email} already exists`});
     }
 
+    //Creating a User document
     user = new User({
       name,
       email,
       password
     });
-
+    
+    //Hashing password
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
 
+    //User document saving
     await user.save();
-    
-    res.json({msg: "User Saved"});
+
+    //Creating payload and getting secret for JWT
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    const jswSecret = config.get("jswSecret");
+
+    //JWT Signature
+    jwt.sign(
+      payload, 
+      jswSecret, {
+      expiresIn: 360000
+    },
+    (e, token) => {
+      if (e) throw new Error(e);
+      res.json({token});
+    });
+
   } catch (e) {
+      //Error handling
       console.error(e.message);
       res.status(500).json({msg: "Server error occured"});
   }
